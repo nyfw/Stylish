4d72ed3b6bd9dacbd8bdffa02a9d95bdfaa67dd8
brandon <41245867+murphybrandon@users.noreply.github.com>
2019-02-08 12:12:35 -0500

    fixed folder structure and enabled css loader

---
+++ styleish/client/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import '../styles/index.css';
+import Nav from './Nav.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      hi
+      < Nav />
+      </div>
+    )
+  }
+}
+
+export default App;
