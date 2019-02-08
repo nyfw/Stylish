bf0f0e15856b1dad021444000180a098edd2594b
Jinsung Park <js.lia.park@nyu.edu>
2019-02-08 17:52:44 -0500

    server

--- styleish/client/components/App.jsx
+++ styleish/client/components/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import '../styles/index.css';
 import Nav from './Nav.jsx';
+import Login from './Login.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
-      hi
-      < Nav />
+        < Nav />
+        <Login />
       </div>
     )
   }
---
+++ styleish/client/components/Opening.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Opening() {
+    return (
+        <div>
+            Opening paragraph here
+    </div>
+    )
+}
+
+export default Opening
+
