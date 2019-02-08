1bf437322aac0ca1454c2d3a8f3d37e1d8ea7b57
Hubert <huberthflin@gmail.com>
2019-02-08 11:08:00 -0500

    added eslint and prettier

--- styleish/client/app/index.js
+++ styleish/client/app/index.js
@@ -1,24 +1,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-require ('./index.css');
-
+require('./index.css');
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    
+
     this.state = {};
   }
 
   render() {
-    return (
-      <div>hi ()=^-^=</div>
-    )
+    return <div>hi ()=^-^=</div>;
   }
 }
 
-
-ReactDOM.render(
-  < App />,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
