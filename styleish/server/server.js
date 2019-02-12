bd5399576792bd660d100b67eda957f697352d5c
Annette Lin <al261310@gmail.com>
2019-02-12 10:08:35 -0500

    auth

--- styleish/server/server.js
+++ styleish/server/server.js
@@ -3,7 +3,6 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { translate } = require('./controllers/translate');
-
 const usersRouter = require('./routers/users');
 
 const PORT = process.env.PORT || 3000;
@@ -11,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extened: true }));
+
 
 app.use('/users', usersRouter);
 
