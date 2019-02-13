94fcad7fe695697ed55b40104bb1cb5259435acd
Daniel Shu <shudaniel95@gmail.com>
2019-02-12 20:49:14 -0500

    DONE WITH MVP

--- styleish/client/components/Ide.jsx
+++ styleish/client/components/Ide.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import brace from 'brace';
-import { split as AceEditor } from 'react-ace';
-import fetch from 'isomorphic-fetch';
-import { TranslateButton } from '../styles/Ide.jsx';
-import 'brace/mode/css';
-import 'brace/theme/xcode';
+import React, { Component } from "react";
+import brace from "brace";
+import { split as AceEditor } from "react-ace";
+import fetch from "isomorphic-fetch";
+import { TranslateButton } from "../styles/Ide.jsx";
+import "brace/mode/css";
+import "brace/theme/xcode";
 
 class Ide extends React.Component {
   constructor(props) {
@@ -13,55 +13,59 @@ class Ide extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.convertToStyled = this.convertToStyled.bind(this);
     this.state = {
-      userInput: '/*Load your css*/ \n \n',
-      converted: '/*Styled component translation here*/ \n \n',
-      example: '/* This is the component you will style and visualize\n<div>\n  <div>\n    <button> </button>\n  </div>\n</div>\n*/ \n \n'
+      userInput: "/*Load your css*/ \n \n",
+      example:
+        "/* This is the component you will style and visualize*/\n<div>\n  <div>\n    <button>\n    </button>\n  </div>\n</div>\n \n \n",
+      converted: "/*Styled component translation here*/ \n \n"
     };
   }
 
   onChange(newValue) {
-    let unparsed = '';
+    // console.log(newValue);
+    let unparsed = "";
     unparsed += newValue[0];
     this.setState({ userInput: unparsed });
+    unparsed = "";
+    unparsed += newValue[1];
+    this.setState({ example: unparsed });
   }
 
   convertToStyled(data) {
     let firstBracket, lastBracket, copiedinfo;
 
     for (let i = 0; i < data.length; i++) {
-      if (data[i] === '{') {
+      if (data[i] === "{") {
         firstBracket = i;
         break;
       }
     }
     for (let i = data.length - 1; i > 0; i--) {
-      if (data[i] === '}') {
+      if (data[i] === "}") {
         lastBracket = i;
         break;
       }
     }
     copiedinfo = data.substring(firstBracket + 1, lastBracket);
     let result =
-      '/*Styled component translation here*/ \n \n const <yourStyledElement> = styled.<tagToStyle>`' +
+      "/*Styled component translation here*/ \n \n const <yourStyledWrapper> = styled.<tagToStyle>`" +
       copiedinfo +
-      '`;';
+      "`;";
     return result;
   }
 
-
-
   handleSubmit() {
     const { userInput } = this.state;
-    fetch('http://localhost:3000/translate', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
+    fetch("http://localhost:3000/translate", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        css: userInput,
+        css: userInput
       })
     })
       .then(response => response.json())
       .then(data => {
         // console.log("data is ", data);
+        this.props.buildTreeStructure(data, this.state.example);
         this.props.changeTreeStructure(data);
         if (data) {
           data = this.convertToStyled(data);
@@ -81,7 +85,11 @@ class Ide extends React.Component {
           theme="xcode"
           onChange={this.onChange}
           name="app"
-          value={[this.state.userInput, this.state.example, this.state.converted]}
+          value={[
+            this.state.userInput,
+            this.state.example,
+            this.state.converted
+          ]}
           splits={3}
           editorProps={{ $blockScrolling: true }}
           showLineNumbers={false}
@@ -91,6 +99,7 @@ class Ide extends React.Component {
           wrapEnabled={true}
           width="700px"
           height="250px"
+          readOnly={false}
         />
 
         <TranslateButton
--- styleish/client/styles/Title.jsx
+++ styleish/client/styles/Title.jsx
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
 export const TitleWrapper = styled.div`
-
   display: flex;
   align-items: center;
   flex-direction: column;
-  background-color: #ECD0EE;
+  background-color: #ecd0ee;
 
   img {
     margin: 50px 0 40px 0;
@@ -17,23 +16,23 @@ export const TitleWrapper = styled.div`
   .tree {
     border: 1px black solid;
     height: 30em;
-    width: 25em;
+    width: 47em;
+    background-color: white;
   }
-`
+`;
 export const Button = styled.button`
   border: 0px;
   border-radius: 10px;
-  background-color: 'white';
+  background-color: "white";
   padding: 0px;
   margin: 0px 20px 40px 20px;
   width: 110px;
   height: 35px;
   display: inline;
-`
+`;
 
 export const AboutText = styled.div`
   margin: 40px 40px 40px 40px;
   max-width: 700px;
   text-align: center;
-
 `;
